875455f3f676f009a4c4c1df175a828d15842612
austinb847 <austinb847@icloud.com>
2020-05-04 16:10:21 -0700

    meaningful fail for leetspeak method

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -9,4 +9,8 @@ describe('String#leetspeak') do
   it('replaces every "e" in a string with a "3"') do
     expect("elephant".leetspeak).to(eq("3l3phant"))
   end
+
+  it('replaces every "o" in a string with a "0"') do
+    expect("boo boo".leetspeak).to(eq("b00 b00"))
+  end
 end
